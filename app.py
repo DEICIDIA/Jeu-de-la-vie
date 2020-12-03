39bd0f3bb0bfe0debf4391bfc5fbdfe5fae09b98
nathan <thoum.nathan@gmail.com>
2020-12-03 18:41:27 +0100

    test p5 js

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ import webbrowser
 from flask import Flask, Response, request, render_template, jsonify
 
 app = Flask(__name__)
+matrice = [[1, 0, 1], [0, 0, 1], [1, 0, 0]]
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/draw')
+def draw():
+
+    reponse = app.response_class(matrice, status = 200)
+    return reponse 
 
 
 app.run(debug = True)
